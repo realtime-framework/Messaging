07efcd2a246addd622daa53986542b44b7c82b7d
João Parreira <jparreira@realtime.co>
2014-11-21 17:50:16 +0000

    Updated UI

--- iOS Push Notifications from the device/CustomPushDemo/ViewController.m
+++ iOS Push Notifications from the device/CustomPushDemo/ViewController.m
@@ -31,4 +31,17 @@
     PushNotification *pushNotification = [[PushNotification alloc] initWithApplicationKey:[_textAppKey text] privateKey:[_textPrivateKey text] channel:[_textChannel text] message:[_textMessage text] andPayload:@{@"sound":[_textSound text], @"badge":[_textBadge text], @"extra": [_textExtra text]}];
     [pushNotification sendPushNotification];
 }
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [_textAppKey resignFirstResponder];
+    [_textPrivateKey resignFirstResponder];
+    [_textChannel resignFirstResponder];
+    [_textMessage resignFirstResponder];
+    
+    [_textBadge resignFirstResponder];
+    [_textSound resignFirstResponder];
+    [_textExtra resignFirstResponder];
+}
+
 @end
